9beb73e332731474983c7b6e51ceccb03845488a
khalid-umar <125467461+khalid-umar@users.noreply.github.com>
2023-10-02 23:14:43 +0530

    HomePage.js

--- HomePage.js
+++ HomePage.js
@@ -1,14 +1,3 @@
-// Complete the HomePage Component and export it
-
-function HomePage() {
-  return (
-    <div className="Homepage">
-      <h1>Home Page</h1>
-    <h3>Login Page</h3>
-    </div>
-  );
-}
-
 import React from 'react';
 import Form from './Form'; // Import the Form component
 
