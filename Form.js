cedcb58a3dc554b389e232842a56bb4f82245fd0
khalid-umar <125467461+khalid-umar@users.noreply.github.com>
2023-10-02 23:07:47 +0530

    Form.js

--- Form.js
+++ Form.js
@@ -1,9 +1,21 @@
-// Complete the Form Component and export it
-
 const Form = () => (
   <>
     <div>
-      <form>{/* Create a h3, 2 inputs and 1 button here */}</form>
+      <form>
+        <h3>Login Page</h3>
+        <div>
+          <label>Name:</label>
+          <input type="text" placeholder="Enter your name" />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input type="email" placeholder="Enter your email" />
+        </div>
+        <button type="submit">Login</button>
+      </form>
     </div>
   </>
 );
+
+export default Form;
+
