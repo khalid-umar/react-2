c6889d84a49a65ff408c812f7735c567a96cecba
khalid-umar <125467461+khalid-umar@users.noreply.github.com>
2023-10-02 23:12:05 +0530

    HomePage.js

--- HomePage.js
+++ HomePage.js
@@ -9,3 +9,16 @@ function HomePage() {
   );
 }
 
+import React from 'react';
+import Form from './Form'; // Import the Form component
+
+function HomePage() {
+  return (
+    <div className="Homepage">
+      <h1>Home Page</h1>
+      <Form /> {/* Render the Form component here */}
+    </div>
+  );
+}
+
+export default HomePage;
